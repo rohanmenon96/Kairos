ec37e435d884a3e852468c421d13123772128d4e
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-03-02 01:47:13 -0500

    Basic Functionality Working

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const path = require("path");
 const axios = require("axios");
 
 const app = express();
-var city = ""
+var city = "";
+var timeArr = [];
+var tempArr = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -29,10 +31,27 @@ app.post("/changeCity",async(req,res)=>{
   console.log("Coming Inside /changeCity route with data ", req.body)
   city = req.body.city;
   const data = await axios.post('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&APPID=e82f066c138586d03df0cc3f61415b63');
+  console.log(data.data.list.length);
+
+  timeArr = [];
+  tempArr = [];
+  let temp;
+
+  for(let i=0; i<data.data.list.length; i++)
+    {
+      timeArr.push(data.data.list[i].dt_txt);
+      temp = data.data.list[i].main.temp;
+      temp = (temp-273.15)*(9/5)+32; 
+      tempArr.push(temp)
+    }
+
+  console.log(timeArr.length);
+  console.log(tempArr.length);
+  
   console.log("\n\nOutput Data: ", data.data.city)
   res.redirect("/");
 })
 
 app.get("/",(req,res)=>{
-  res.render(path.join(__dirname, "/views/index.handlebars"), {"city" : city})
+  res.render(path.join(__dirname, "/views/index.handlebars"), {"city" : city, "timeArr": timeArr, "tempArr": tempArr})
 })
